0168daa4d2de050fb3f23662c438d3bd8d4a1760
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 16:38:00 -0700

    Updated OOP lesson - recloned directory.

--- game.py
+++ game.py
@@ -13,9 +13,28 @@ GAME_WIDTH = 3
 GAME_HEIGHT = 3
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    rock_positions = [
+        (2,1),
+        (1,2),
+        (3,2),
+        (2,3)
+    ]
+
+    rocks = []
+
+    for position in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(position[0], position[1], rock)
+        rocks.append(rock)
+
+    for rock in rocks:
+        print rock
+
+
