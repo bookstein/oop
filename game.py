63284b5398635479a5966264748f93c2f3c5329d
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 16:49:23 -0700

    Restored game to previous stage with keyboard messages

--- game.py
+++ game.py
@@ -9,12 +9,29 @@ GAME_BOARD = None
 DEBUG = False
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
 class Rock(GameElement):
     IMAGE = "Rock"
+
+class Character(GameElement):
+    IMAGE = "Princess"
+
+    def keyboard_handler(self, symbol, modifier):
+        if symbol == key.UP:
+            self.board.draw_msg("%s says: 'You pressed up!'") % self.IMAGE
+        elif symbol == key.SPACE:
+            self.board.erase_msg()
+        elif symbol == key.DOWN:
+            self.board.draw_msg("%s says: 'You pressed down!'") % self.IMAGE
+        elif symbol == key.LEFT:
+            self.board.draw_msg("%s says: 'You pressed left!'") % self.IMAGE
+        elif symbol == key.RIGHT:
+            self.board.draw_msg("%s says: 'You pressed right!'") % self.IMAGE
+        else:
+            self.board.draw_msg("%s says: 'I don't get it!'") % self.IMAGE
 ####   End class definitions    ####
 
 def initialize():
@@ -36,5 +53,12 @@ def initialize():
 
     for rock in rocks:
         print rock
+
+    player = Character()
+    GAME_BOARD.register(player)
+    GAME_BOARD.set_el(2,2,player)
+    print player
+
+    GAME_BOARD.draw_msg("This game is wicked awesome.")
 
 
