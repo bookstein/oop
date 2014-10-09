c9fb188d0492cb1fa9468cba827b3300709792cd
Hackbright Student <info@hackbrightacademy.com>
2014-10-09 15:05:58 -0700

    Game works, but cat does not interact

--- game.py
+++ game.py
@@ -70,7 +70,7 @@ class Character(GameElement):
                 existing_el = self.board.get_el(next_x, next_y)
                 if existing_el:
                     existing_el.interact(self)
-                    
+
                 if existing_el and existing_el.SOLID:
                     self.board.draw_msg("There's something in my way!")
 
@@ -83,17 +83,67 @@ class NewCat(GameElement):
     IMAGE = 'Cat'
     SOLID = True
 
+
+    def move_character(self, direction):
+        if direction:
+            next_location = self.next_pos(direction)
+        
+
+
+
+            if next_location:
+                next_x = next_location[0]
+                next_y = next_location[1]
+                existing_el = self.board.get_el(next_x, next_y)
+                
+                if exisiting_el.IMAGE == 'Cat':
+                    exisiting_el.interact(playe) 
+                    self.board.draw_msg("There's something in my way!")
+
+                if existing_el:
+                    existing_el.interact(self)
+
+                if existing_el and existing_el.SOLID:
+                    self.board.draw_msg("There's something in my way!")
+
+
+
+
+
+                elif existing_el is None or not existing_el.SOLID:
+                    self.board.del_el(self.x, self.y)
+                    self.board.set_el(next_x, next_y, self)
+
+
+
+
+
+
+
+
+
+
+
     def __init__(self):
         self.inventory = []
 
     def interact(self, player):
+        
+
+        # update players position when it interacts with cat                                               0000000000000000000000000000000000-`q`
+
+
+
+
+
+
         if len(player.inventory) > 0:
             gem = player.inventory.pop()
             self.inventory.append(gem)
 
             GAME_BOARD.draw_msg("The CAT just acquired a gem! The cat have %d items! CATITUDE"%(len(self.inventory)))
         else:
-            GAME_BOARD.draw_msg("Ouch! The cat sratched you. Go away!")
+            GAME_BOARD.draw_msg("Ouch! The cat scratched you. Go away!")
 
 
 class NewFriend (Character):
